b6700783d43308f81323023100ab12a3662712ff
JoaoHFerreira <jaycops@gmail.com>
2023-01-26 14:29:46 -0300

    repplace in main file the for the module

---
+++ msgs.py
@@ -0,0 +1,50 @@
+def main_menu():
+    print("\n\n|-------------------------------------|")
+    print("|Digite 1 para apontar horas          |")
+    print("|Digite 2 para visualizar apontamentos|")
+    print("|Digite 3 para instruções             |")
+    print("|Digite 4 para sair                   |")
+    print("|-------------------------------------|\n\n")
+
+
+def roll_back_menu():
+    print("\n\n")
+    print("\n\n|-------------------------------------------------|")
+    print("|Digite 1 para finalizar                           |")
+    print("|Digite outro número para voltar ao menu principal |")
+    print("|-------------------------------------------------|\n\n")
+
+
+def dynamic_or_static_menu():
+    print("\n\n|-------------------------------------|")
+    print("|Digite 1 para apontar dinamicamente  |")
+    print("|Digite 2 para apontar estaticamente|")
+    print("|-------------------------------------|\n\n")
+
+
+def activity_description():
+    print("\n\n", "Digite uma breve descrição da atividade:")
+
+
+def activity_max_time():
+    print("O tempo não pode ser >= 12 horas")
+
+
+def max_time_in_minutes():
+    print("O tempo máximo em minutos eh '59'")
+
+
+def total_time_in_hours():
+    print("\n\n", "Tempo da atividade em horas")
+
+
+def total_time_in_minutes():
+    print("\n\n", "Tempo da atividade em minutos")
+
+
+def hour_init_time():
+    print("\n\n", "Digite a hora de inicio da atividade")
+
+
+def miniute_init_time():
+    print("\n\n", "Digite minutos de inicio do trabalho:")
